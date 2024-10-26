d42ce47463ad100d887e5c4b6e32eba9ec431946
username <Jzazzi56@gmail.com>
2024-10-25 23:57:12 -0700

    remvoing depercted code

--- config/mongodb.js
+++ config/mongodb.js
@@ -3,10 +3,7 @@ require("dotenv").config();
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect(process.env.MONGODB_URI);
     console.log("You are connected to your Mongo database.");
   } catch (error) {
     console.log("unable to connect to MongoDB", error);
