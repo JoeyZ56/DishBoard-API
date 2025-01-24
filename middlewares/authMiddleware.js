84fdd6103d8c51b60e0c9f5336c1e1b7a248cf8c
username <Jzazzi56@gmail.com>
2025-01-23 16:33:29 -0800

    protecting routes

---
+++ middlewares/authMiddleware.js
@@ -0,0 +1,23 @@
+const admin = require("firebase-admin");
+
+const verifyToken = async (req, res, next) => {
+  // Accesses the Authorization header from the incoming HTTP request.
+  const token = req.headers.authorization?.split(" ")[1];
+
+  if (!token) {
+    return res
+      .status(401)
+      .json({ message: "Unauthorized: No token provided!" });
+  }
+
+  try {
+    const decodedToken = await admin.auth().verifyIdToken(token);
+    req.user = decodedToken; // Attach user info to request
+    next(); // Continue to the next middleware or route handler
+  } catch (error) {
+    console.error("Failed to process token:", error.message);
+    return res.status(401).json({ message: "Unauthorized: Invalid token!" });
+  }
+};
+
+module.exports = verifyToken;
