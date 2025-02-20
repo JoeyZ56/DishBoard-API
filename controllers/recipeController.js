fdf833996fa54bfca5dc9da8b1ee46421f4cfb50
JoeyZ56 <jzazzi56@gmail.com>
2025-02-19 16:55:15 -0800

    added controller functions for GET: getrecipe category and get all recipes

--- controllers/recipeController.js
+++ controllers/recipeController.js
@@ -24,4 +24,33 @@ const createRecipe = async (req, res) => {
   }
 };
 
-module.exports = createRecipe;
+//GET all recipes
+const getAllRecipes = async (req, res) => {
+  try {
+    const recipes = await Recipe.find();
+    res.status(200).json(recipes);
+  } catch (error) {
+    res.status(500).json({ message: "Error fetching recipes", error });
+  }
+}
+
+//get recipes by category
+const getRecipesByCategory = async (req, res) => {
+  try {
+    const {category} = req.params;
+    const recipes = await Recipe.find({ category: category });
+
+    // Check if recipes are found
+    if (!recipes || recipes.length === 0) {
+      return res.status(400).json({ message: "No recipes found under this category"})
+    } else {
+      res.status(200).json(recipes);
+    }
+  } catch (error) {
+    console.error("error fetching recipes by category:", error);
+    res.status(500).json({ message: "Error fetching recipes by category", error });S
+  }
+}
+
+// search recipes by name
+module.exports = {createRecipe, getAllRecipes, getRecipesByCategory};
