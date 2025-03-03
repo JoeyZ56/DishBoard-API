7cee4d1aac5d100c8723e720a924d86bf203a8fa
JoeyZ56 <jzazzi56@gmail.com>
2025-03-03 15:03:45 -0800

    added prepTime, cook temp, and serving count to the model

--- models/recipe.js
+++ models/recipe.js
@@ -5,6 +5,9 @@ const recipeSchema = new mongoose.Schema(
     name: { type: String, required: true },
     fileUpload: { type: Buffer, required: true },
     ingredients: { type: String, required: true },
+    prepTime: { type: String, required: true },
+    cookTime: { type: String, required: true },
+    servings: { type: String, required: true },
     instructions: { type: String, required: true },
     category: {
       type: String,
