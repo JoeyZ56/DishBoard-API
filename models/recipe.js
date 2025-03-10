228ff88d56886a2e67491b767504167159976595
JoeyZ56 <jzazzi56@gmail.com>
2025-03-10 14:26:58 -0700

    changed name to recipeName

--- models/recipe.js
+++ models/recipe.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const recipeSchema = new mongoose.Schema(
   {
-    name: { type: String, required: true },
+    recipeName: { type: String, required: true },
 
     category: {
       type: String,
