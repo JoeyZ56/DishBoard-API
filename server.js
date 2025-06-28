80d1738201cc9f17535d11a92670794110459499
username <Jzazzi56@gmail.com>
2025-06-28 14:03:00 -0700

    reverting code back to original and ddeleting vercel.js, now hosting API on railway

--- server.js
+++ server.js
@@ -71,10 +71,8 @@ app.use("/api/recipes", recipeRoute);
 
 app.use("/api/users", userRoutes);
 
-if (require.main === module) {
-  app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
-  });
-}
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
 
 module.exports = app;
