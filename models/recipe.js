f41624830854e3c6ff7ba266df145ef17ac062e7
username <Jzazzi56@gmail.com>
2025-07-01 13:57:44 -0700

    removing recipe id and relying on Mongo DB's unique id it provides

--- models/recipe.js
+++ models/recipe.js
@@ -2,9 +2,6 @@ const mongoose = require("mongoose");
 
 const recipeSchema = new mongoose.Schema(
   {
-    // rid stands for recipe id, similar to uid in the user model
-    rid: { type: String, required: true, unique: true },
-
     recipeName: { type: String, required: true },
 
     courseType: {
