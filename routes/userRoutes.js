1d02fa75e99caa0b87251ad306ef112bd301d482
username <Jzazzi56@gmail.com>
2025-07-12 15:52:29 -0700

    omg, how many places did i put verifyToken? im brain dead and blind

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -4,6 +4,7 @@ const {
   signInWithEmailAndPassword,
   adminAuth,
 } = require("../firebase/firebaseConfig");
+const verifyToken = require("../firebase/firebaseAuth");
 const {
   updateUsername,
   getUserByUid,
@@ -42,25 +43,6 @@ router.post("/", async (req, res) => {
       .json({ message: "Failed to create new User", error: error.message });
   }
 });
-
-//Verify Firebase Tokens on Protected Routes
-const verifyToken = async (req, res, next) => {
-  const token = req.headers.authorization?.split(" ")[1];
-
-  if (!token) {
-    return res
-      .status(401)
-      .json({ message: "Unauthorized: No token provided!" });
-  }
-
-  try {
-    const decodedToken = await admin.auth().verifyIdToken(token);
-    req.user = decodedToken;
-    next();
-  } catch (error) {
-    return res.status(401).json({ message: "Unauthorized: Invalid token!" });
-  }
-};
 
 //Protect sensitive Routes
 router.get("/profile", verifyToken, async (req, res) => {
